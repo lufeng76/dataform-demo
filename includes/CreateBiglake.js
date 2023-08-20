2217752328f2ee2b29ce73a5e83967095f0afcf5
Feng Lu <lufengsh@google.com>
2023-08-20 01:03:14 +0000

    second commit

---
+++ includes/CreateBiglake.js
@@ -0,0 +1,17 @@
+/*
+The function CreateBiglake() takes as input two arguments: table name and gcs file path,
+and calls a BQ stored procedure to create a biglake table
+* You can learn more about functions on https://cloud.google.com/dataform/docs/reuse-code-includes
+*/
+
+
+function CreateBiglake(name, uris) {
+
+
+return `call dataform_demo.create_biglake_table(${name},"${uris}")`;
+}
+
+
+module.exports = {
+CreateBiglake
+};
